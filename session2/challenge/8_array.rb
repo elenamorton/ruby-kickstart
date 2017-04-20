c9f8ffa0557b0ee80a3690bb90be4e408b849be8
Elena Morton <elena.uleia@gmail.com>
2017-04-20 09:01:33 +0000

    Submit solution for 8_array.rb

--- session2/challenge/8_array.rb
+++ session2/challenge/8_array.rb
@@ -5,3 +5,14 @@
 # got_three? ['a', 'a', 'b']  # => false
 # got_three? ['a', 'a', 'a']  # => true
 # got_three? [1, 2, 1, 1]     # => false
+
+def got_three?(arr)
+    has_three = false
+    
+    arr.each_cons(3) do |a, b, c|
+        if a == b && a == c
+           has_three = true
+        end
+    end
+    has_three
+end

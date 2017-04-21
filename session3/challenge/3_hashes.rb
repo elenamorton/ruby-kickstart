fecf3a4209ced364954f7dd65efc77f22ce480f7
Elena Morton <elena.uleia@gmail.com>
2017-04-21 20:01:56 +0000

    Submit the solution for 3_hashes.rb.

--- session3/challenge/3_hashes.rb
+++ session3/challenge/3_hashes.rb
@@ -6,3 +6,13 @@
 #
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
+
+def word_count(str)
+    counts = Hash.new { |my_hash, num| my_hash[num] = 0 }
+    
+    str.downcase.split(" ").each do |word|
+        counts[word] += 1
+    end   
+    counts
+    
+end

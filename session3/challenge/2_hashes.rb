f412dcf74b8b7d59c66adbaec457df7fe6d1e73b
Elena Morton <elena.uleia@gmail.com>
2017-04-21 19:40:24 +0000

    Submit the solution for 2_hashes.rb.

--- session3/challenge/2_hashes.rb
+++ session3/challenge/2_hashes.rb
@@ -8,3 +8,26 @@
 # staircase 4  # => {1 => [], 3 => [2]}
 # staircase 5  # => {1 => [], 3 => [2], 5 =>[2, 4]}
 
+def staircase(n)
+    
+    results = {}
+    i = 1
+
+    while i <= n do
+        if i.odd? && i > 2
+            local_arr = Array(1..i)
+            local_arr.select! { |i| i.even? }
+            results[i] = local_arr
+        elsif i <= 2
+            local_arr = []
+            results[1] = local_arr
+        end
+        
+        i += 1
+    end
+    
+    results
+   # p results
+end
+
+p staircase(5)

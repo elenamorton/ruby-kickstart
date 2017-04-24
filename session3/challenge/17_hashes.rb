aec926f28e78ebf5c4ed5d84bf33db099f841b84
Elena Morton <elena.uleia@gmail.com>
2017-04-24 11:26:34 +0000

    Submit solution for 17_hashs.rb

--- session3/challenge/17_hashes.rb
+++ session3/challenge/17_hashes.rb
@@ -7,3 +7,17 @@
 #
 # print_list_in_reverse head   # >> "1\n2\n"
 
+def print_list_in_reverse(list)
+    output = []
+    while list
+        output << list[:data]
+        list = list[:next]
+    end
+    puts output.reverse!
+end
+
+
+head = {:data => 1, :next => nil}
+print_list_in_reverse head
+head = {:data => 2, :next => head}
+print_list_in_reverse head
